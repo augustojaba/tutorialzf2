df548638e92bf12a92c2efdf1531ac33d0039520
augusto <augusto@augusto-note>
2014-04-17 02:03:15 -0300

    Criação do módulo controller contato por completo

---
+++ public/js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * efeito alert
+ */
+
+$(function () {
+    // pegar elemento com corpo da mensagem
+    var corpo_alert = $("#alert-message");
+    // verificar se o elemento esta presente na página
+    if(corpo_alert.length)
+        corpo_alert.fadeOut().fadeIn().fadeOut().fadeIn();
+});
